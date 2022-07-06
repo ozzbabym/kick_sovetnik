d273712bb17c8ce8c7bef513087748718c1e3e09
Ivan Nazarov <ivan.nazarov@gridnine.com>
2022-07-06 09:48:32 +0300

    create script

---
+++ kick_yandex_sovetnik.js
@@ -0,0 +1,27 @@
+function kickSovetnik () {
+    var elems = $('body div[id]');
+    var styles = $('style');
+
+
+    Object.keys(elems).forEach(function(item) {
+        if (/[a-z0-9]{11,13}$/.test(elems[item].id)) {
+            elems[item].remove();
+        }
+    });
+
+    Object.keys(styles).forEach(function(item) {
+        if (styles[item].style && styles[item].innerHTML.includes('38px !important;')) {
+            styles[item].remove();
+        }
+    });
+
+    $('html').css({margintTop: 0});
+};
+
+observer = new MutationObserver( kickSovetnik );
+var  options = {
+    'childList': true
+};
+var body = document.querySelector( 'body' );
+
+observer.observe( body , options );
